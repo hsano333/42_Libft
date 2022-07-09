9a6981da2de92bfcbf90e3d3a86e6cfeb8c5346f
hsano <hsano@student.42tokyo.jp>
2022-07-10 02:48:50 +0900

    complete ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/10 01:07:11 by hsano             #+#    #+#             */
+/*   Updated: 2022/07/10 02:48:35 by hsano            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t	max;
+	double	*p;
+	char	*c;
+	size_t	i;
+	max = count*size;
+	p = (double*)malloc(max);
+	c = (char *)p;
+	i = max/(sizeof(double)/sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	while(i--)
+		p[i] = 0;
+
+	c = (char *)p;
+	i = 8;
+	while(i--)
+		c[max-i] = 0;
+	return (void*)(c);
+}
+//#include <stdio.h>
+//#include <time.h>
+//int main(int argc, char** argv)
+//{
+//	char *s1;
+//	char *s2;
+//	size_t count;
+//	size_t size;
+//	clock_t start_clock1, end_clock1;
+//	clock_t start_clock2, end_clock2;
+//	size_t	max;
+//
+//	printf("%zu\n",sizeof(double)/sizeof(char));
+//	if (argc == 3)
+//	{
+//		count = (size_t)atoi(argv[1]);
+//		size = (size_t)atoi(argv[2]);
+//		max = count*size;
+//
+//		start_clock1 = clock();
+//		s2= calloc(count,size);
+//		end_clock1 = clock();
+//		printf("test time:%ld\n",end_clock1-start_clock1);
+//		while(max--)
+//		{
+//			if(s2[max] != 0)
+//				printf("error s2\n");
+//		}
+//
+//
+//		start_clock2 = clock();
+//		s1= ft_calloc(count,size);
+//		end_clock2 = clock();
+//		printf("my time:%ld\n",end_clock2-start_clock2);
+//
+//		max = count*size;
+//		while(max--)
+//		{
+//			if(s1[max] != 0)
+//				printf("error s1\n");
+//		}
+//	}
+//	return (0);
+//}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 21:40:57 by hsano             #+#    #+#             */
-/*   Updated: 2022/07/09 22:42:00 by hsano            ###   ########.fr       */
+/*   Updated: 2022/07/10 01:07:02 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_calloc(size_t count, size_t size);
         	
 size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
 size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
@@ -57,7 +58,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
      
      
 	     
-     
+     	 
       
 	      
 
