4c1f571682b91986b7a5f6786b5592b27a01f495
hsano <hsano@student.42tokyo.jp>
2022-07-12 00:01:28 +0900

    fix ft_atoi but must fix norm error

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 22:42:24 by hsano             #+#    #+#             */
-/*   Updated: 2022/07/10 14:43:39 by hsano            ###   ########.fr       */
+/*   Updated: 2022/07/12 00:00:22 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	get_len(char *str)
+size_t	get_len(char *str)
 {
 	int	i;
 
@@ -27,47 +27,81 @@ int	get_len(char *str)
 	return (i);
 }
 
+char	*delete_blank(char *str)
+{
+	while(TRUE)
+	{
+		if (*str == ' ' || (*str >= 0x09 && *str <= 0x0D))
+			str++;
+		else
+		{
+			if (*str == '+')
+				str++;
+			break;
+		}
+	}
+	return (str);
+}
+
 int		ft_atoi(const char *str)
 {
-	int sum;
-	int a;
-	int	i;
+	unsigned long	sum;
+	unsigned long	old;
+	long	a;
+	size_t	i;
+	char	*p;
 
+	p = delete_blank((char *)str);
 	a = 1;
 	sum = 0;
-	i	= get_len((char *)str);
+	i= get_len((char *)p);
 	while(--i > 0)
 	{
-		sum += (str[i] - '0') * a;
+		sum += (p[i] - '0') * a;
 		a *= 10;
+		if (sum > LONG_MAX)
+		{
+			if ((p[0] == '-') || (sum -1  == (LONG_MAX)))
+			//return (0);
+				return (0);
+			else
+				return (-1);
+		}
 	}
-	if (str[0] == '-')
+	if (p[0] == '-')
 		sum *= -1;
-	else if('0' <= str[0] && str[0] <= '9')
-		sum += (str[0] - 0x30) * a;
+	else if('0' <= p[0] && p[0] <= '9')
+	{
+		old = sum;
+		sum += (p[0] - 0x30) * a;
+		if (old > sum)
+			return (-1);
+	}
 	else
-		sum = 0;
-	return (sum);
+		return (0);
+	return (int)(sum);
 }
-//#include <stdio.h>
-//#include <string.h>
-//#include <stdlib.h>
-//int main(int argc,char **argv)
-//{
-//	char *s1;
-//	int len;
-//	int	result;
-//
-//	result = 0;
-//	if (argc == 2)
-//	{
-//		s1 = argv[1];
-//		len = atoi(s1);
-//		result = ft_atoi(s1);
-//		printf("input:%s\n",s1);
-//		printf("my:%d\n",result);
-//		printf("test:%d\n",len);
-//
-//	}
-//	return (0);
-//}
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(int argc,char **argv)
+{
+	char *s1;
+	int len;
+	int	result;
+
+	result = 0;
+	printf("LONGMAX=%ld\n",LONG_MAX);
+	if (argc == 2)
+	{
+		s1 = argv[1];
+		//s1 = "\t\v\f\r\n \f-06050";
+		len = atoi(s1);
+		result = ft_atoi(s1);
+		printf("input:%s\n",s1);
+		printf("my:%d\n",result);
+		printf("test:%d\n",len);
+
+	}
+	return (0);
+}
