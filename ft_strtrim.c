0f8eb551d79764d2b63357b2872a33524c802795
hsano <hsano@student.42tokyo.jp>
2022-07-12 13:54:38 +0900

    fixing ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 18:36:03 by hsano             #+#    #+#             */
-/*   Updated: 2022/07/11 02:55:09 by hsano            ###   ########.fr       */
+/*   Updated: 2022/07/12 13:21:57 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,7 +90,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 		{
 			i += j;
 			while (j--)
-				p[i - len + j + 1] = set[j];
+				p[i - j - 1] = set[j];
 			p[i++] = s1[k];
 			j = 0;
 		}
@@ -100,16 +100,21 @@ char	*ft_strtrim(char const *s1, char const *set)
 		p[i + j] = set[j];
 	return (p);
 }
-//int main(int argc, char **argv)
-//{
-//
-//	char *p;
-//	if (argc == 3)
-//	{
-//		p = ft_strtrim(argv[1],argv[2]);
-//		printf("input1=%s,input2=%s\n",argv[1],argv[2]);
-//		printf("output=%s\n",p);
-//
-//	}
-//	return (0);
-//}
+int main(int argc, char **argv)
+{
+
+	char *p;
+	if (argc == 3)
+	{
+		char	*s1 = "   \t  \n\n \t\t  \n\n\nHello \t  Please\n Trim me !\n   \n \n \t\t\n  ";
+		char	*s2 = " \n\t";
+		p = ft_strtrim(argv[1],argv[2]);
+		printf("input1=%s,input2=%s\n",argv[1],argv[2]);
+		printf("output=%s:end\n",p);
+		p = ft_strtrim(s1,s2);
+		printf("input1=%s,input2=%s\n",s1,s2);
+		printf("output=%s:end\n",p);
+
+	}
+	return (0);
+}
