1fbed67bc520c04e659f128a8960d1c75ebfad28
hsano <hsano@student.42tokyo.jp>
2022-07-13 04:40:44 +0900

    fix ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,11 +6,11 @@
 /*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 18:37:16 by hsano             #+#    #+#             */
-/*   Updated: 2022/07/12 03:06:03 by hsano            ###   ########.fr       */
+/*   Updated: 2022/07/13 04:34:36 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -20,9 +20,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 
 	i = 0;
 	k = 0;
-	len_n = 0;
-	while (((char *)needle)[len_n])
-		len_n++;
+	len_n = ft_strlen(needle);
 	while (i < len)
 	{
 		if (len_n == k)
@@ -30,33 +28,36 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		else if (haystack[i] == needle[k])
 			k++;
 		else
+		{
+			i-=k;
 			k = 0;
+		}
 		i++;
 	}
 	if (len_n == k)
 		return (&(((char *)haystack)[i - len_n]));
 	return (0);
 }
-//#include <stdio.h>
-//#include <string.h>
-//#include <stdlib.h>
-//int main(int argc,char **argv)
-//{
-//	char *s1;
-//	char *s2;
-//	char *result;
-//	size_t		len;
-//	if (argc == 4)
-//	{
-//		s1 = argv[1];
-//		s2 = argv[2];
-//		len = atoi(argv[3]);
-//		result = ft_strnstr(s1,s2,len);
-//		printf("s1=%s,s2=%s,len=%zu\n",s1,s2,len);
-//		printf("result=%s\n",result);
-//		result = strnstr(s1,s2,len);
-//		printf("result=%s\n",result);
-//
-//	}
-//	return (0);
-//}
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(int argc,char **argv)
+{
+	char *s1;
+	char *s2;
+	char *result;
+	size_t		len;
+	if (argc == 4)
+	{
+		s1 = argv[1];
+		s2 = argv[2];
+		len = atoi(argv[3]);
+		result = ft_strnstr(s1,s2,len);
+		printf("s1=%s,s2=%s,len=%zu\n",s1,s2,len);
+		printf("result=%s\n",result);
+		result = strnstr(s1,s2,len);
+		printf("result=%s\n",result);
+
+	}
+	return (0);
+}
