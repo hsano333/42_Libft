895c4fa676770e1565f6868ab494e5066a8796e8
hsano <hsano@student.42tokyo.jp>
2022-07-13 15:02:08 +0900

    fix ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,41 +6,50 @@
 /*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 22:42:24 by hsano             #+#    #+#             */
-/*   Updated: 2022/07/13 12:41:22 by hsano            ###   ########.fr       */
+/*   Updated: 2022/07/13 15:01:44 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	get_len(char *str)
+size_t	get_digit_len(const char *str, size_t i)
+{
+	size_t	len;
+
+	len = i;
+	while (str[len])
+	{
+		if ('0' <= str[len] && str[len] <= '9')
+			len++;
+		else
+			break ;
+	}
+	return (len - i);
+}
+
+size_t	get_start_index(const char *str, int *minus)
 {
 	size_t	i;
 
-	i = 1;
-	while (str[i])
-	{
-		if ('0' <= str[i] && str[i] <= '9')
-			i++;
-		else
-			break ;
-	}
-	return (i);
-}
-
-char	*delete_blank(char *str)
-{
+	i = 0;
+	*minus = 1;
 	while (TRUE)
 	{
-		if (*str == ' ' || (0x09 <= *str && *str <= 0x0D))
-			str++;
+		if (str[i] == ' ' || (0x09 <= str[i] && str[i] <= 0x0D))
+			i++;
 		else
 		{
-			if (*str == '+')
-				str++;
+			if (str[i] == '+')
+				i++;
+			else if (str[i] == '-')
+			{
+				i++;
+				*minus = -1;
+			}
 			break ;
 		}
 	}
-	return (str);
+	return (i);
 }
 
 int	last_check(char *p, unsigned long sum, unsigned long a)
@@ -61,52 +70,77 @@ int	last_check(char *p, unsigned long sum, unsigned long a)
 	return (sum);
 }
 
+unsigned long	ft_iterative_power(long nb, long power)
+{
+	long	rval;
+	long	old;
+
+	rval = (long)nb;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	if (nb == 0)
+		return (0);
+	while (--power)
+	{
+		old = rval;
+		rval = nb * rval;
+		if (old > rval)
+			return (LONG_MAX);
+	}
+	return (rval);
+}
+
 int	ft_atoi(const char *str)
 {
+	int		minus;
+	size_t		len;
+	size_t		i;
 	unsigned long	sum;
 	unsigned long	a;
-	size_t			i;
-	char			*p;
 
-	p = delete_blank((char *)str);
-	a = 1;
+	i = get_start_index(str, &minus);
 	sum = 0;
-	i = get_len((char *)p);
-	while (--i > 0)
+	len = get_digit_len(str, i);
+	if (len == 0)
+		return (0);
+	a = ft_iterative_power(10, len - 1);
+	while (a > 0)
 	{
-		sum += (p[i] - '0') * a;
-		a *= 10;
+		sum += (str[i++] - '0') * a;
 		if (sum > LONG_MAX)
 		{
-			if ((p[0] == '-') || (sum - 1 == (LONG_MAX)))
+			if (minus == -1 || sum == (LONG_MAX))
 				return (0);
 			else
 				return (-1);
 		}
+		a /= 10;
 	}
-	return ((int)(last_check(p, sum, a)));
+	return ((int)(sum * minus));
 }
-// #include <stdio.h>
-// #include <string.h>
-// #include <stdlib.h>
-// int main(int argc,char **argv)
-// {
-// 	char *s1;
-// 	int len;
-// 	int	result;
-// 
-// 	result = 0;
-// 	printf("LONGMAX=%ld\n",LONG_MAX);
-// 	if (argc == 2)
-// 	{
-// 		s1 = argv[1];
-// 		//s1 = "\t\v\f\r\n \f-06050";
-// 		len = atoi(s1);
-// 		result = ft_atoi(s1);
-// 		printf("input:%s\n",s1);
-// 		printf("my:%d\n",result);
-// 		printf("test:%d\n",len);
-// 
-// 	}
-// 	return (0);
-// }
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(int argc,char **argv)
+{
+	char *s1;
+	int len;
+	int	result;
+
+	result = 0;
+	printf("LONGMAX=%ld\n",LONG_MAX);
+	if (argc == 2)
+	{
+		s1 = argv[1];
+		//s1 = "\t\v\f\r\n \f-06050";
+		len = atoi(s1);
+		result = ft_atoi(s1);
+		printf("input:%s\n",s1);
+		printf("my:%d\n",result);
+		printf("test:%d\n",len);
+
+	}
+	return (0);
+}
