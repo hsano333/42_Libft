c62cb22bcb26a4e579f36fa1389241084cfcdf09
hsano <hsano@student.42tokyo.jp>
2022-07-14 22:57:36 +0900

    complete ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,28 +6,79 @@
 /*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 13:12:09 by hsano             #+#    #+#             */
-/*   Updated: 2022/07/14 21:00:14 by hsano            ###   ########.fr       */
+/*   Updated: 2022/07/14 22:56:57 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+static size_t	get_count(char *p, int n)
 {
-	char	*p;
-	//int		i;
-	int		a;
-	//int		b;
-	//char	tmp[20];
+	long	a;
+	size_t	i;	
+	size_t	cnt;
 
-	a = n / 10;
-	/*
+	i = 0;
+	cnt = 0;
+	if (n < 0)
+	{
+		a = -1 * (long)n;
+		cnt++;
+	}
+	else
+		a = n;
 	while (a)
 	{
-		//tmp[i] = (n % 10);
-		a = n / 10;
+		p[i++] = (a % 10) + '0';
+		a /= 10;
+		cnt++;
 	}
-	*/
-	p = malloc(n);
+	if (n < 0)
+		p[i++] = '-';
+	return (cnt);
+}
+
+static char	*str_zero(void)
+{
+	char	*p;
+
+	p = malloc(2);
+	if (!p)
+		return (NULL);
+	p[0] = '0';
+	p[1] = '\0';
 	return (p);
 }
+
+char	*ft_itoa(int n)
+{
+	size_t	i;
+	char	p[20];
+	char	*r;
+	size_t	cnt;
+
+	if (n == 0)
+		return (str_zero());
+	cnt = get_count(p, n);
+	r = malloc(cnt + 1);
+	if (!r)
+		return (NULL);
+	r[cnt] = '\0';
+	i = 0;
+	while (i < cnt)
+	{
+		r[i] = p[cnt - i - 1];
+		i++;
+	}
+	return (r);
+}
+//int main(int argc, char **argv)
+//{
+//	char *p;
+//
+//	if (argc == 2)
+//	{
+//		p = ft_itoa(ft_atoi(argv[1]));
+//		printf("p = %s\n",p);
+//	}
+//}
