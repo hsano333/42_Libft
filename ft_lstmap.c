5d5525915bbdd7418b8e636028ca3aded5178459
hsano <hsano@student.42tokyo.jp>
2022-07-15 22:17:44 +0900

    complete ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,21 +6,36 @@
 /*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 00:40:22 by hsano             #+#    #+#             */
-/*   Updated: 2022/07/15 01:14:04 by hsano            ###   ########.fr       */
+/*   Updated: 2022/07/15 22:17:32 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list *tmp;
+	t_list	*bk;
+	t_list	*tmp;
 
+	if (!f || !lst)
+		return (NULL);
 	tmp = (t_list *)malloc(sizeof(t_list));
-	tmp->next = lst;
-	f(tmp->next);
-	del(tmp->next);
-
-
-	return tmp;
+	if (!tmp)
+		return (NULL);
+	bk = tmp;
+	tmp->content = f(lst->content);
+	while (lst->next)
+	{
+		tmp->next = (t_list *)malloc(sizeof(t_list));
+		if (!tmp->next)
+		{
+			if (del)
+				ft_lstclear(&bk, del);
+			return (NULL);
+		}
+		tmp->next->content = f(lst->content);
+		lst = lst->next;
+		tmp = tmp->next;
+	}
+	return (bk);
 }
