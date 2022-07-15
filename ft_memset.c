38d6b9e35d3be9f64237f0052b6fd96ed6526b54
hsano <hsano@student.42tokyo.jp>
2022-07-16 03:56:31 +0900

    speed up ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,18 +6,49 @@
 /*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 22:29:36 by hsano             #+#    #+#             */
-/*   Updated: 2022/07/13 04:05:21 by hsano            ###   ########.fr       */
+/*   Updated: 2022/07/16 03:56:11 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	*memset_big_data(void *b, int c, size_t len, size_t i)
+{
+	size_t				loop;
+	unsigned long long	tmp;
+	unsigned char		tmp_c;
+	size_t				tmp_len;
+	unsigned long long	*p;
+
+	p = (unsigned long long *)b;
+	loop = sizeof(unsigned long long) / sizeof(unsigned char);
+	tmp_len = len / loop;
+	tmp_c = (unsigned char)c;
+	tmp = tmp_c;
+	i = loop - 1;
+	while (i > 0)
+		tmp = tmp | (unsigned long long)tmp_c << ((i--) * 8);
+	i = 0;
+	while (i < tmp_len)
+		p[i++] = tmp;
+	b = (unsigned char *)p;
+	tmp_len = (len / loop) * loop;
+	while (tmp_len < len)
+		((unsigned char *)b)[tmp_len++] = c;
+	return ((unsigned char *)b);
+}
+
 void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t	i;
+	size_t			i;
 
 	i = 0;
-	while (i < len)
-		((unsigned char *)b)[i++] = c;
-	return (b);
+	if (len > 32)
+		return (memset_big_data(b, c, len, i));
+	else
+	{
+		while (i < len)
+			((unsigned char *)b)[i++] = c;
+		return (b);
+	}
 }
