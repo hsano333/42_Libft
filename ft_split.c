fcbba4e94811a4599e2dbfa2022a0e5058606057
hsano <hsano@student.42tokyo.jp>
2022-07-17 23:31:16 +0900

    fix ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 12:58:17 by hsano             #+#    #+#             */
-/*   Updated: 2022/07/14 20:21:33 by hsano            ###   ########.fr       */
+/*   Updated: 2022/07/17 23:30:17 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@ static size_t	count_c(char const *s, char c)
 {
 	size_t	i;
 	size_t	cnt;
-	int		flag;
+	int		match_flag;
 
-	i = 0;
+	i = 1;
 	cnt = 0;
-	flag = true;
+	match_flag = true;
 	if (c == '\0')
 		return (1);
 	while (s[i])
 	{
 		if (s[i] == c)
-			flag = true;
-		else if (flag == true)
+			match_flag = true;
+		else if (match_flag == true)
 		{
-			flag = false;
+			match_flag = false;
 			cnt++;
 		}
 		i++;
@@ -37,51 +37,63 @@ static size_t	count_c(char const *s, char c)
 	return (cnt);
 }
 
-static char	*write_array(char const *s, size_t diff, size_t i)
+static char	*write_array(char const *s, size_t diff, size_t i, int *error)
 {
 	char	*p;
 
 	p = malloc(diff + 1);
+	if (!p)
+	{
+		*error = true;
+		return (NULL);
+	}
 	p[diff] = '\0';
 	while (diff--)
 		p[diff] = s[i + diff];
 	return (p);
 }
 
+static char	**last_check(char **p, int error, size_t j)
+{
+	size_t	i;
+
+	if (error)
+	{
+		i = 0;
+		while (i < j)
+		{
+			free(p[i]);
+			i++;
+		}
+	}
+	return (p);
+}
+
 static char	**make_array(char **p, char const *s, int c, size_t cnt)
 {
 	char	*tmp_p;
+	int		error;
 	size_t	i;
 	size_t	j;
 	size_t	diff;
 
 	j = 0;
 	i = 0;
-	while (j < cnt)
+	error = false;
+	while (j < cnt && !error)
 	{
 		tmp_p = ft_strchr(&(s[i]), c);
 		if (tmp_p == NULL)
 			break ;
 		diff = tmp_p - &(s[i]);
 		if (diff > 0)
-			p[j++] = write_array(s, diff, i);
+			p[j++] = write_array(s, diff, i, &error);
 		i += diff + 1;
 	}
 	diff = ft_strlen(s) - i;
-	if (j < cnt)
-		p[j] = write_array(s, diff, i);
-	return (p);
-}
-
-static char	**allocate_memmory(size_t cnt)
-{
-	char	**p;
-
-	p = (char **)malloc(sizeof(char *) * (cnt + 1));
-	if (p == NULL)
-		return (NULL);
-	p[cnt] = NULL;
-	return (p);
+	if (j < cnt && !error)
+		p[j] = write_array(s, diff, i, &error);
+	return (last_check(p, error, j));
 }
 
 char	**ft_split(char const *s, char c)
@@ -89,12 +101,21 @@ char	**ft_split(char const *s, char c)
 	char	**p;
 	size_t	cnt;
 
-	if (s == NULL || s[0] == '\0')
-		return (allocate_memmory(0));
+	if (s == NULL)
+		return (NULL);
+	if (s[0] == '\0')
+	{
+		p = (char **)malloc(sizeof(char *) * 1);
+		if (p == NULL)
+			return (NULL);
+		p[0] = NULL;
+		return (p);
+	}
 	cnt = count_c(s, c);
-	p = allocate_memmory(cnt);
+	p = (char **)malloc(sizeof(char *) * (cnt + 1));
 	if (p == NULL)
 		return (NULL);
+	p[cnt] = NULL;
 	p = make_array(p, s, (int)c, cnt);
 	return (p);
 }
