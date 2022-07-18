d7494842e04778ce42b9af176c3f0647027e4763
hsano <hsano@student.42tokyo.jp>
2022-07-18 23:03:37 +0900

    fix timeout ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,36 +6,52 @@
 /*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 00:40:22 by hsano             #+#    #+#             */
-/*   Updated: 2022/07/17 02:52:49 by hsano            ###   ########.fr       */
+/*   Updated: 2022/07/18 23:02:37 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+t_list	*allocate_memory_list(t_list *lst, void (*del)(void *))
+{
+	size_t	cnt;
+	t_list	*p;
+	t_list	*tmp;
+
+	cnt = ft_lstsize(lst);
+	p = NULL;
+	while (cnt--)
+	{
+		tmp = (t_list *)malloc(sizeof(t_list));
+		if (!tmp)
+		{
+			if (del)
+				ft_lstclear(&p, del);
+			return (NULL);
+		}
+		tmp->next = p;
+		p = tmp;
+	}
+	return (p);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*bk;
-	t_list	*tmp;
+	t_list	*p;
 
 	if (!f || !lst)
 		return (NULL);
-	tmp = (t_list *)malloc(sizeof(t_list));
-	if (!tmp)
+	p = allocate_memory_list(lst, del);
+	if (!p)
 		return (NULL);
-	bk = tmp;
-	tmp->content = f(lst->content);
+	bk = p;
+	p->content = f(lst->content);
 	while (lst->next)
 	{
-		tmp->next = (t_list *)malloc(sizeof(t_list));
-		if (!tmp->next)
-		{
-			if (del)
-				ft_lstclear(&bk, del);
-			return (NULL);
-		}
 		lst = lst->next;
-		tmp->next->content = f(lst->content);
-		tmp = tmp->next;
+		p = p->next;
+		p->content = f(lst->content);
 	}
 	return (bk);
 }
